797610843b3e4c6c131a1cf8f9508322c0eb4e5c
KaidenGroves <85904637+Kaiden-Groves@users.noreply.github.com>
2021-06-22 20:12:12 +0000

    Firstchange

--- script.js
+++ script.js
@@ -1,13 +1,69 @@
 // Author:
 
 // Global UI Variables
+let canvasDiv;
+
+let canvas;
+
+let textDiv;
+
+let textP;
+
+let buttonDiv;
+
+let uploadButton;
+
+let submitButton;
+
+let resetButton;
 //let canvasDiv;
 
 // Global ML Variables
+let mobilenet;
+
+let img;
 //let mobilenet;
+canvasDiv = createDiv();
+
+canvas = createCanvas(640, 480);
+
+canvas.parent(canvasDiv);
+
+textDiv = createDiv();
+
+textP = createP("Model loading, please wait...");
+
+textP.parent(textDiv);
+
+textP.html("Upload an image to classify!");
+
+uploadButton.style("display", "inline");
 
 function setup() {
+  mobilenet = ml5.imageClassifier("MobileNet", modelReady);
+  buttonDiv = createDiv();
 
+uploadButton = createFileInput(handleFile);
+
+uploadButton.parent(buttonDiv);
+
+uploadButton.style("display", "none");
+
+submitButton = createButton("SUBMIT");
+
+submitButton.parent(buttonDiv);
+
+submitButton.mousePressed(predictImage);
+
+submitButton.style("display", "none");
+
+resetButton = createButton("RESET");
+
+resetButton.parent(buttonDiv);
+
+resetButton.mousePressed(resetCanvas);
+
+resetButton.style("display", "none");
 }
 
 function draw() {
@@ -19,7 +75,13 @@ function modelReady() {
 }
 
 function resetCanvas() {
+  background(255);
 
+  submitButton.style("display", "none");
+  
+  modelReady();
+
+resetButton.style("display", "none")
 }
 
 function handleFile(file) {
@@ -31,13 +93,34 @@ function handleFile(file) {
 }
 
 function imageReady() {
+  image(img, 0, 0, width, height);
+  submitButton.style("display", "inline");
 
+resetButton.style("display", "inline");
+
+uploadButton.style("display", "none");
+
+textP.html("Image ready for classification!");
 }
 
 function predictImage() {
-
+  mobilenet.classify(canvas, gotResults);
 }
 
 function gotResults(error, results) {
 
+  if(error) {
+
+    console.error(error);
+
+  } else {
+    submitButton.style("display", "none");
+    
+
+  }
+  let label = results[0].label;
+
+  let confidence = round(results[0].confidence, 2);
+
+  textP.html("Label: " + label + " - Confidence " + confidence);
 }
